36ebbaed000f5a626d777187da2cf3d048ae5a41
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-04-10 19:45:10 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="max-w-screen-xl mx-auto bg-zinc-700 py-6">
+      <img
+        src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg"
+        alt=""
+      />
+      <div className="links"></div>
+    </div>
+  );
+}
+
+export default Navbar;
