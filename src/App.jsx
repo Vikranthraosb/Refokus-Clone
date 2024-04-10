9aaee2161ff71f499fc5c06b2e75f08bd0b45d55
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-04-10 23:29:49 +0530

    completed work section. Moving towords Stipe section now

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Work from "./components/Work";
 function App() {
   return (
     <div className="w-full h-screen bg-zinc-900 text-white">
       <Navbar />
+      <Work />
     </div>
   );
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+function Work() {
+  var images = [
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
+      top: "50%",
+      left: "50%",
+      isactive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png",
+      top: "56%",
+      left: "47%",
+      isactive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png",
+      top: "45%",
+      left: "56%",
+      isactive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef092455ce2cf591e52d1_Rainfall.png",
+      top: "52%",
+      left: "40%",
+      isactive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png",
+      top: "57%",
+      left: "57%",
+      isactive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png",
+      top: "60%",
+      left: "59%",
+      isactive: false,
+    },
+  ];
+  return (
+    <div className="w-full">
+      <div className="relative max-w-screen-xl bg-zinc-700 mx-auto mt-2 p-6">
+        <h1 className="text-[23vw] leading-none tracking-tight font-semibold text-center select-none">
+          WORK
+        </h1>
+        <div className="absolute top-0 w-full h-full">
+          {images.map(
+            (elem, index) =>
+              elem.isactive && (
+                <img
+                  className="absolute w-60 rounded-lg -translate-x-[50%] -translate-y-[50%]"
+                  src={elem.url}
+                  style={{ top: elem.top, left: elem.left }}
+                  alt=""
+                />
+              )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Work;
