e1513bd139c5b2352e9f5bef3d4e5d21f56ad918
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-04-11 16:28:22 +0530

    Moving Towards Animation Part, Smooth Srolling through Locomotive js

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Stripes from "./components/Stripes";
 import Products from "./components/Products";
 import Marquees from "./components/Marquees";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import LocomotiveScroll from "locomotive-scroll";
+
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className="w-full min-h-screen bg-zinc-900 text-white">
       <Navbar />
@@ -14,6 +19,7 @@ function App() {
       <Products />
       <Marquees />
       <Cards />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="w-full">
+      <div className="max-w-screen-xl py-10 flex gap-32 mx-auto">
+        <div className="basis-1/2">
+          <h1 className="text-9xl font-semibold leading-none tracking-tight">
+            Refokus.
+          </h1>
+        </div>
+        <div className="basis-1/2 flex gap-4">
+          <div className="basis-1/3">
+            <h1 className="mb-10 text-zinc-400 capitalize text-lg">Socials</h1>
+            {["instagram", "twitter", "Linkdin"].map((item, index) => (
+              <a href="" className="block m-2 capitalize text-zinc-500">
+                {item}
+              </a>
+            ))}
+          </div>
+          <div className="basis-1/3">
+            <h1 className="mb-10 text-zinc-400 capitalize text-lg">Socials</h1>
+            {["instagram", "twitter", "Linkdin"].map((item, index) => (
+              <a href="" className="block m-2 capitalize text-zinc-500">
+                {item}
+              </a>
+            ))}
+          </div>
+          <div className="basis-1/2">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
